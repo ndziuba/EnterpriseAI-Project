ac66ce1ed7a2b92d37c33247bfc2413059aaeb22
m3ndell <83732214+m3ndell@users.noreply.github.com>
2023-07-06 17:43:25 +0200

    Update training_pipeline.py

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -20,4 +20,3 @@ def training_pipeline(path: str='data', batch_size: int = 32 , epochs: int = 5):
     train_ds, valid_ds, test_ds = data_loader.data_loader(path=path, batch_size=batch_size)
     model = trainer.resnet_trainer(train_ds=train_ds, valid_ds=valid_ds, epochs=epochs)
     evaluator.model_evaluator(test_ds=test_ds, model=model)
-    data_loader.data_loader()
