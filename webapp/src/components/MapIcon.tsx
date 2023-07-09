4966c54c6b1c31545b4307107ca771bd9320d001
Nicholas Dziuba <nicholasdziuba@icloud.com>
2023-07-10 00:27:40 +0200

    move files

--- webapp/src/components/MapIcon.tsx
+++ webapp/src/components/MapIcon.tsx
@@ -1,13 +1,10 @@
 import { useEffect } from 'react';
-import * as L from 'leaflet';
+import L from 'leaflet';
 
 const MapIcon = () => {
   useEffect(() => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-explicit-any
-    const Leaflet = L as any;
-
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    Leaflet.Icon.Default.mergeOptions({
+    // @ts-expect-error type definition is missing for leaflet 
+    L.Icon.Default.mergeOptions({
       iconRetinaUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
       iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
       shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
