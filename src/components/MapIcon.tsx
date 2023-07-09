3681423fdf0f52766d8596a5d16ebe1bae799c6a
Nicholas Dziuba <nicholasdziuba@icloud.com>
2023-07-10 00:01:02 +0200

    vercel second try

--- src/components/MapIcon.tsx
+++ src/components/MapIcon.tsx
@@ -3,7 +3,7 @@ import L from 'leaflet';
 
 const MapIcon = () => {
   useEffect(() => {
-    // @ts-expect-error type definition is missing
+    // @ts-expect-error type definition is missing for leaflet default icon
     L.Icon.Default.mergeOptions({
       iconRetinaUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
       iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
