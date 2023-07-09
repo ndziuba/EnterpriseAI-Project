9d1fec1dc8ad00a02ea5a8deb3af9aeff12f78e7
Nicholas Dziuba <nicholasdziuba@icloud.com>
2023-07-10 00:07:05 +0200

    pls vercel

--- src/components/MapIcon.tsx
+++ src/components/MapIcon.tsx
@@ -3,9 +3,10 @@ import * as L from 'leaflet';
 
 const MapIcon = () => {
   useEffect(() => {
-    // Explicitly cast L to any
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-explicit-any
     const Leaflet = L as any;
 
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
     Leaflet.Icon.Default.mergeOptions({
       iconRetinaUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
       iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
