7c0e949e4852f24aabf0ca21114d364816256925
Nicholas Dziuba <nicholasdziuba@icloud.com>
2023-07-10 00:05:05 +0200

    cast to any instead of ignoring error

--- src/components/MapIcon.tsx
+++ src/components/MapIcon.tsx
@@ -1,10 +1,12 @@
 import { useEffect } from 'react';
-import L from 'leaflet';
+import * as L from 'leaflet';
 
 const MapIcon = () => {
   useEffect(() => {
-    // @ts-expect-error type definition is missing for leaflet default icon
-    L.Icon.Default.mergeOptions({
+    // Explicitly cast L to any
+    const Leaflet = L as any;
+
+    Leaflet.Icon.Default.mergeOptions({
       iconRetinaUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
       iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
       shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
