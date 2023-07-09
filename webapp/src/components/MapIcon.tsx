cb6932cf251f0c55b991fec9bddf9c18e589d004
Nicholas Dziuba <nicholasdziuba@icloud.com>
2023-07-10 00:37:34 +0200

    finally

--- webapp/src/components/MapIcon.tsx
+++ webapp/src/components/MapIcon.tsx
@@ -3,7 +3,6 @@ import L from 'leaflet';
 
 const MapIcon = () => {
   useEffect(() => {
-    // @ts-expect-error type definition is missing for leaflet 
     L.Icon.Default.mergeOptions({
       iconRetinaUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
       iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
