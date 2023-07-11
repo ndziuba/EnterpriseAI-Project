da7d05d6da9a951a14d6e4a50206efe0ee2c900b
Tim Lachner <45331740+miTTimmiTTim@users.noreply.github.com>
2023-07-11 10:36:02 +0200

    Changed the data_loader to add wildfire images from the canadian site to the datasets within a time delta.

--- steps/data_loader.py
+++ steps/data_loader.py
@@ -1,89 +1,30 @@
-
-import tensorflow as tf
-from tensorflow.python.lib.io import file_io
-from tensorflow.keras.preprocessing import image_dataset_from_directory
-from tensorflow.image import stateless_random_flip_left_right
-import logging
-from zenml import pipeline, step
-from zenml.steps import Output
-from typing import Type
-from zenml.materializers.base_materializer import BaseMaterializer
-from zenml.enums import ArtifactType
-import os
-
-class TensorFlowDatasetMaterializer(BaseMaterializer):
-    ASSOCIATED_TYPES = (tf.data.Dataset,)
-    ASSOCIATED_ARTIFACT_TYPE = ArtifactType.DATA
-
-    def load(self, data_type: Type[tf.data.Dataset]) -> tf.data.Dataset:
-        """Read from artifact store."""
-        return tf.data.experimental.load(self.uri)
-
-    def save(self, dataset: tf.data.Dataset) -> None:
-        """Write to artifact store."""
-        tf.data.experimental.save(dataset, self.uri)
-
-
+import pandas as pd
+import requests
+from PIL import Image
+from io import BytesIO
+import datetime 
 """
 Import the data using tf.image_dataset_from_directory.
 """
-@step(enable_cache=False, output_materializers=TensorFlowDatasetMaterializer)
-def data_loader(path: str='data', batch_size: int=32) -> Output(
-    train_ds=tf.data.Dataset,
-    valid_ds=tf.data.Dataset,
-    test_ds=tf.data.Dataset,
-    ):
+@step(enable_cache=False)
+def data_loader(path: str='data', daydelta: int=3) -> int:
     logging.info("Loading raw data")
-    train_ds = image_dataset_from_directory(
-        directory = path+"/train",
-        seed = 1324,
-        label_mode = 'categorical',
-        image_size = (350, 350),
-        batch_size=None
-    )  
-    valid_ds = image_dataset_from_directory(
-        directory = path+"/valid",
-        seed = 1324,
-        label_mode = 'categorical',
-        image_size = (350, 350),
-        batch_size=None
-    )
-    test_ds = image_dataset_from_directory(
-        directory = path+"/test",
-        seed = 1324,
-        label_mode = 'categorical',
-        image_size = (350, 350),
-        batch_size=None
-    )
+    url="https://cwfis.cfs.nrcan.gc.ca/downloads/activefires/activefires.csv"
+    canada_wilfires=pd.read_csv(url)
+    cut_date = datetime.datetime.today() - datetime.timedelta(days=1)
+    canada_wilfires = canada_wilfires.drop(canada_wilfires[canada_wilfires[' startdate'] == " "].index)
+    canada_wilfires[' startdate'] = pd.to_datetime(canada_wilfires[' startdate'])
+    relevant = canada_wilfires[canada_wilfires[' startdate']> cut_date]
+    url = 'https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/{},{},15,0/350x350'
+    access_token = 'pk.eyJ1IjoibTNuZGVsIiwiYSI6ImNsajV5a2syMjAxOW8zam9ibXp4dGFkNmQifQ.PA3LPn0Ihn_xJ-tOnEUsWg'
+    ending = '&attribution=false&logo=false'
+    count = 0
+    for index, row in relevant.iterrows():
+        count += 1
+        response = requests.get(url.format(row[' lon'],row[' lat']) + '?access_token=' + access_token + ending)
+        img = Image.open(BytesIO(response.content))
+        img.save(f"data/additional/wildfire/{row[' lon']},{row[' lat']}.jpg")
+    return count
+
+
     
-    additional_ds = image_dataset_from_directory(
-        directory = path+"/additional",
-        seed = 1324,
-        label_mode = 'categorical',
-        image_size = (350, 350),
-        batch_size=None
-    )
-    logging.info("Divide additional data")
-    train_ds = train_ds.concatenate(additional_ds.take(int(len(additional_ds)*0.6)))
-    valid_ds = valid_ds.concatenate(additional_ds.skip(int(len(additional_ds)*0.6))
-                                     .take(int(len(additional_ds)*0.2)))
-    test_ds = test_ds.concatenate(additional_ds.skip(int(len(additional_ds)*0.6))
-                                   .skip(int(len(additional_ds)*0.2))
-                                   .take(int(len(additional_ds)*0.2)))
-    # logging.info("Apply augmentation")
-    # train_ds = train_ds.map(
-    #                  lambda image, label: (stateless_random_flip_left_right(image, seed=(1, 2)) if label[1] == 1 else image, label)
-    #            )
-    # valid_ds = valid_ds.map(
-    #                  lambda image, label: (stateless_random_flip_left_right(image, seed=(1, 2)) if label[1] == 1 else image, label)
-    #            )
-    # valid_ds = valid_ds.map(
-    #                  lambda image, label: (stateless_random_flip_left_right(image, seed=(1, 2)) if label[1] == 1 else image, label)
-    #          )
-    
-    logging.info("Batching")
-    train_ds = train_ds.batch(batch_size)
-    valid_ds = valid_ds.batch(batch_size)
-    test_ds = test_ds.batch(batch_size)
-    logging.info("DATA LOADER DONE")
-    return train_ds, valid_ds, test_ds
