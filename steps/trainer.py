74c6c855d8833f1db229ddbe6f25f02213e33c96
Tim Lachner <45331740+miTTimmiTTim@users.noreply.github.com>
2023-07-14 14:07:25 +0200

    Trainer update comments

--- steps/trainer.py
+++ steps/trainer.py
@@ -1,83 +1,64 @@
 import tensorflow as tf
 import logging
 from tensorflow.keras import Sequential
-from zenml import step
-from tensorflow.keras.layers import Flatten
-from keras.layers.core import Dense
+from tensorflow.keras.layers import Flatten, Dense
 from tensorflow.keras.preprocessing import image_dataset_from_directory
 from tensorflow.keras.optimizers import Adam
 from zenml.integrations.tensorflow.materializers.keras_materializer import KerasMaterializer
 import mlflow
 
-"""
-Train a modified ResNet50 model.
-"""
 @step(output_materializers=KerasMaterializer, experiment_tracker='mlflow_experiment_tracker', enable_cache=True) 
-def resnet_trainer(epochs: int, path: str, batch_size:int
-) -> tf.keras.Model:
-    logging.info("Loading train/valid/additional data")
+def resnet_trainer(epochs: int, path: str, batch_size:int) -> tf.keras.Model:
+    """
+    This function trains a ResNet50 model with modified top layers for a specific task.
+    
+    Args:
+        epochs (int): The number of training epochs.
+        path (str): The path to the dataset directory.
+        batch_size (int): The batch size used for the training.
+        
+    Returns:
+        model (tf.keras.Model): The trained Keras model.
+    """
+    logging.info("Loading train and validation data")
+
     train_ds = image_dataset_from_directory(
-        directory = path+"/train",
+        directory = f"{path}/train",
         seed = 1324,
         label_mode = 'categorical',
         image_size = (350, 350),
-        #rescale=1./255,
         batch_size=batch_size
-    )  
+    )
+
     valid_ds = image_dataset_from_directory(
-        directory = path+"/valid",
-        seed = 1324,
-        label_mode = 'categorical',
-        image_size = (350, 350),
-        #rescale=1./255,
-        batch_size=batch_size
-    )
-
-    """
-    additional_ds = image_dataset_from_directory(
-        directory = path+"/additional",
+        directory = f"{path}/valid",
         seed = 1324,
         label_mode = 'categorical',
         image_size = (350, 350),
         batch_size=batch_size
     )
-    data_augmentation = tf.keras.Sequential([
-        layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical")])
-    train_ds = train_ds.map(lambda x, y: (data_augmentation(x, training=True), y))
-    valid_ds = valid_ds.map(lambda x, y: (data_augmentation(x, training=True), y))
 
+    logging.info("Setting up the model architecture")
 
-    train_ds = train_ds.concatenate(additional_ds.take(int(len(additional_ds)*0.6)))
-    valid_ds = valid_ds.concatenate(additional_ds.skip(int(len(additional_ds)*0.6))
-                                     .take(int(len(additional_ds)*0.2)))
-    train_ds = train_ds.shuffle(buffer_size=1000)
-    valid_ds = valid_ds.shuffle(buffer_size=1000)
-    """
     model = Sequential()
-
-    # import pretrained model
     resnet_model = tf.keras.applications.ResNet50(
-    include_top=False,
-    input_shape=(350,350,3),
-    pooling='avg',
-    weights='imagenet'
+        include_top=False,
+        input_shape=(350,350,3),
+        pooling='avg',
+        weights='imagenet'
     )
-
-    # exclude pretrained model weights from being recalculated
     for layer in resnet_model.layers:
         layer.trainable = False
 
-    # add pretrained ResNet50 model to sequential model
     model.add(resnet_model)
-
-    # add additional layers to model
     model.add(Flatten())
-    model.add(Dense(256, tf.nn.relu))
-    model.add(Dense(2, tf.nn.softmax))
-    
-    # train model
+    model.add(Dense(256, activation=tf.nn.relu))
+    model.add(Dense(2, activation=tf.nn.softmax))
+
+    logging.info("Starting model training")
+
     model.compile(optimizer=Adam(), loss='binary_crossentropy', metrics=['accuracy'])
-    mlflow.tensorflow.autolog()
+    mlflow.tensorflow.autolog()  # Ensure MLflow is tracking this model's training
     model.fit(train_ds, validation_data=valid_ds, epochs=epochs)
 
     return model
