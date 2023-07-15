e9d503d1c8f2f9699dc95ea4367dcca2ec1b0ee7
m3ndell <mennni24@gmail.com>
2023-07-15 16:19:08 +0200

    fix

--- service.py
+++ service.py
@@ -12,7 +12,6 @@ runner = bentoml.tensorflow.get("wf_model:latest").to_runner()
 
 svc = bentoml.Service(name="wf_service", runners=[runner])
 
-service_version = bentoml.get("wf_service:latest").__getattribute__('tag').version
 
 @svc.api(input=Image(), output=NumpyNdarray())
 async def predict_image(f: PILImage) -> NDArray[Any]:
@@ -21,4 +20,6 @@ async def predict_image(f: PILImage) -> NDArray[Any]:
     img_batch = tf.expand_dims(img_tensor , 0)
     
     arr = await runner.async_run(img_batch)
-    return np.append(arr, model_version)
+
+    service_version = svc.__getattribute__('tag').version
+    return np.append(arr, service_version)
