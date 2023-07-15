d951eb10a64ba90cc0cc69518cb891fe028766e9
Nicholas Dziuba <nicholas.dziuba@stud-mail.uni-wuerzburg.de>
2023-07-15 14:55:18 +0200

    Update get_bento_version.py

--- get_bento_version.py
+++ get_bento_version.py
@@ -1,7 +1,12 @@
 from minio import Minio
+from dotenv import load_dotenv
+from pathlib import Path
 
-client = Minio( "s3.eai.dziubalabs.de", secure=True)
-objects = client.list_objects("yatai", prefix="bentos/default/wf_classifier", recursive=True)
+dotenv_path = Path('./k8s/.env')
+load_dotenv(dotenv_path=dotenv_path)
+
+client = Minio( "$DOMAIN", secure=True)
+objects = client.list_objects("yatai", prefix="bentos/default/wf_service", recursive=True)
 objects = sorted(objects, key=lambda obj: obj.last_modified, reverse=True)
 latest_object = objects[0]
 bento_tag = latest_object.object_name.split('/')[-1].split('.')[0]
