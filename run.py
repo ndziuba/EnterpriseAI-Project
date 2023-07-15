0eb01666a6db9d1d37b2feca570914b17244aba6
m3ndell <mennni24@gmail.com>
2023-07-15 16:32:04 +0200

    added option to schedule pipeline execution

--- run.py
+++ run.py
@@ -1,9 +1,28 @@
 from pipelines.training_pipeline import training_pipeline
+import datetime
+from datetime import timedelta
+from zenml.pipelines import Schedule
 
 def main(path: str='data', batch_size: int=32, epochs: int=5, hp_tuning_epochs: int=1):
     """Run the training  pipeline."""
 
+
     training_pipeline(path=path, batch_size=batch_size, epochs=epochs, hp_tuning_epochs=hp_tuning_epochs)
+
+
+    '''
+    #
+    # run the pipeline on a schedule once every day for 5 weeks (remove comment formatting in code for scheduled execution)
+    #
+
+    schedule = Schedule(
+        start_time=datetime.now(),
+        end_time=datetime.now() + timedelta(weeks=5),
+        interval_second=86.400,
+    )
+
+    training_pipeline(path=path, batch_size=batch_size, epochs=epochs, hp_tuning_epochs=hp_tuning_epochs).run(schedule=schedule)
+    '''
 
 if __name__ == "__main__":
     main()
